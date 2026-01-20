ae11959cd286cca23fe07a824f235898f6f4d232
バーチャ農ちゃんねる <vach.catering235@passmail.net>
2026-01-20 20:48:50 +0900

    feat: Refactor MeasurementModule to `base.py` and disable all CLI functionality, directing users to the GUI.

--- src/measurement_modules/base.py
+++ src/measurement_modules/base.py
@@ -3,18 +3,30 @@ from abc import ABC, abstractmethod
 
 
 class MeasurementModule(ABC):
+    """
+    Base class for all measurement modules.
+    
+    Provides interfaces for both GUI (get_widget) and CLI (run).
+    Note: CLI functionality (run method) is currently suspended/frozen.
+    """
     @property
     @abstractmethod
     def name(self) -> str:
+        """Module name used for identification and CLI command."""
         pass
 
     @property
     @abstractmethod
     def description(self) -> str:
+        """Brief description of the module's purpose."""
         pass
 
     @abstractmethod
     def run(self, args: argparse.Namespace):
+        """
+        Execute the measurement from CLI.
+        Currently frozen/not implemented for most modules.
+        """
         pass
 
     def get_widget(self):
