4b3c198df6ed0b73bff4e9fb04e92c0b5fc1455b
バーチャ農ちゃんねる <vach.catering235@passmail.net>
2026-03-03 16:04:06 +0900

    feat: Implement Lock-in Spectrum Finder and add new UI localization strings across multiple languages.

--- src/gui/widgets/lockin_spectrum_finder.py
+++ src/gui/widgets/lockin_spectrum_finder.py
@@ -330,7 +330,7 @@ class LockInSpectrumFinderWidget(QWidget):
 
         # Mode Selection
         self.combo_mode = QComboBox()
-        self.combo_mode.addItems(["Basic", "Zoom"])
+        self.combo_mode.addItems([tr("Basic"), tr("Zoom")])
         self.combo_mode.setCurrentText(self.module.mode)
         self.combo_mode.currentTextChanged.connect(self.on_mode_changed)
         form.addRow(tr("Mode:"), self.combo_mode)
@@ -373,7 +373,7 @@ class LockInSpectrumFinderWidget(QWidget):
 
         self.lbl_spacing = QLabel(tr("Spacing:"))
         self.combo_spacing = QComboBox()
-        self.combo_spacing.addItems(["Log", "Lin"])
+        self.combo_spacing.addItems([tr("Log"), tr("Lin")])
         self.combo_spacing.setCurrentText(self.module.spacing)
         self.combo_spacing.currentTextChanged.connect(self.on_spacing_changed)
         form.addRow(self.lbl_spacing, self.combo_spacing)
@@ -451,7 +451,7 @@ class LockInSpectrumFinderWidget(QWidget):
         
         current_val = str(self.module.buffer_size)
         
-        if self.module.mode == "Basic":
+        if self.module.mode == tr("Basic"):
             # Up to 512k (approx 500k)
             options = ["65536", "131072", "262144", "524288"]
         else:
@@ -478,7 +478,7 @@ class LockInSpectrumFinderWidget(QWidget):
         self.combo_buffer.blockSignals(False)
 
     def _update_plot_log_mode(self):
-        if self.module.mode == "Basic" and self.module.spacing == "Log":
+        if self.module.mode == tr("Basic") and self.module.spacing == tr("Log"):
             self.plot.getPlotItem().setLogMode(x=True, y=False)
         else:
             self.plot.getPlotItem().setLogMode(x=False, y=False)
@@ -557,7 +557,7 @@ class LockInSpectrumFinderWidget(QWidget):
 
         self.sweep_line.show()
         val = freqs[0]
-        if self.module.mode == "Basic" and self.module.spacing == "Log" and val > 0:
+        if self.module.mode == tr("Basic") and self.module.spacing == tr("Log") and val > 0:
             val = np.log10(val)
         self.sweep_line.setValue(val)
         self.lbl_status.setText(tr("Calculating... 0%"))
@@ -568,7 +568,7 @@ class LockInSpectrumFinderWidget(QWidget):
         if hasattr(self, 'sweep_line'):
             self.sweep_line.show()
             val = f_chunk[-1]
-            if self.module.mode == "Basic" and self.module.spacing == "Log" and val > 0:
+            if self.module.mode == tr("Basic") and self.module.spacing == tr("Log") and val > 0:
                 val = np.log10(val)
             self.sweep_line.setValue(val)
         pct = int((end_idx / len(self.current_freqs)) * 100)
